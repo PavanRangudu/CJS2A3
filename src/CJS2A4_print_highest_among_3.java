95fc058a6d96463f36ab3d2a551174a77e2018a1
PavanRangudu <pavanharishankar@gmail.com>
2017-03-11 10:10:37 +0530

    CJS2A4_solution

---
+++ src/CJS2A4_print_highest_among_3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CJS2A4_print_highest_among_3 {
+
+	public static void main(String s[]) {
+		Scanner input = new Scanner(System.in);
+		double a,b,c; 
+		System.out.println("Enter 3 numbers :"); 
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		if (a>b) {
+			if (a>c)
+				System.out.println(a);
+			else
+				System.out.println(c);
+		}
+		else if (b>c) {
+			System.out.println(b);
+		} else
+			System.out.println(c);
+		input.close(); // closing the input 
+	}
+
+}
